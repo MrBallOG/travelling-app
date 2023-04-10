dfc52fe7113c5f856641641448867cb200b7620b
Hubert Piłka <hubert.pilka@outlook.com>
2023-04-10 16:33:43 +0200

    retaking picture

--- mobile/lib/src/camera/camera_view.dart
+++ mobile/lib/src/camera/camera_view.dart
@@ -26,6 +26,7 @@ class _CameraViewState extends State<CameraView> with WidgetsBindingObserver {
   }
 
   Future<XFile> getPicture() async {
+    final width = MediaQuery.of(context).size.width;
     final response = await picker.retrieveLostData();
 
     if (!response.isEmpty &&
@@ -33,18 +34,10 @@ class _CameraViewState extends State<CameraView> with WidgetsBindingObserver {
         response.type == RetrieveType.image) {
       return Future.value(response.file);
     } else {
-      return Future.value(await picker.pickImage(source: ImageSource.camera));
+      return Future.value(
+          await picker.pickImage(source: ImageSource.camera, maxWidth: width));
     }
   }
-
-  // Future<void> pictureCallback(BuildContext context, String imagePath) async {
-  //   await Future.value(Navigator.restorablePushNamed(
-  //       context, PictureView.routeName,
-  //       arguments: imagePath));
-  //   // Navigator.restorablePushNamed(context, PictureView.routeName,
-  //   //     arguments: imagePath);
-  //   futurePicture = getPicture();
-  // }
 
   @override
   Widget build(BuildContext context) {
@@ -56,46 +49,49 @@ class _CameraViewState extends State<CameraView> with WidgetsBindingObserver {
         future: futurePicture,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            //return Image.file(File(snapshot.data!.path));
-            // WidgetsBinding.instance.addPostFrameCallback((_) =>
-            //     Navigator.restorablePushNamed(context, PictureView.routeName,
-            //         arguments: snapshot.data!.path));
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+            return Stack(
               children: [
-                Image.file(File(snapshot.data!.path)),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    SizedBox(
-                      height: 60,
-                      width: 60,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(60.0),
+                Center(child: Image.file(File(snapshot.data!.path))),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(60, 0, 60, 32.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        SizedBox(
+                          height: 60,
+                          width: 60,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(60.0),
+                              ),
+                              backgroundColor: Colors.red,
+                            ),
+                            child: const Icon(Icons.close),
+                            onPressed: () => setState(() {
+                              futurePicture = getPicture();
+                            }),
                           ),
-                          backgroundColor: Colors.red,
                         ),
-                        child: const Icon(Icons.close),
-                        onPressed: () => {},
-                      ),
-                    ),
-                    SizedBox(
-                      height: 60,
-                      width: 60,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(60.0),
+                        SizedBox(
+                          height: 60,
+                          width: 60,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(60.0),
+                              ),
+                              backgroundColor: Colors.green,
+                            ),
+                            child: const Icon(Icons.check),
+                            onPressed: () => {},
                           ),
-                          backgroundColor: Colors.green,
                         ),
-                        child: const Icon(Icons.check),
-                        onPressed: () => {},
-                      ),
+                      ],
                     ),
-                  ],
+                  ),
                 ),
               ],
             );
