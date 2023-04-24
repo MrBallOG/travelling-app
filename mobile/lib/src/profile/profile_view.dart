baa63e34363c708fecc98beab0f418f63bab4c56
Hubert Piłka <hubert.pilka@outlook.com>
2023-04-24 18:44:31 +0200

    unused import

--- mobile/lib/src/profile/profile_view.dart
+++ mobile/lib/src/profile/profile_view.dart
@@ -2,7 +2,6 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile/src/settings/settings_view.dart';
-import 'package:path/path.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key});
